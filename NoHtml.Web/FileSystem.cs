ecfac78c62e459b1d4c321496ddb0ba6c5301add
Patrick Huber <prhuber@gmail.com>
2014-03-01 15:40:05 -0500

    adds passing filesystem tests

---
+++ NoHtml.Web/FileSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NoHtml.Web
+{
+    public class FileSystem : IFileSystem
+    {
+        public System.IO.Stream OpenRead(string path)
+        {
+            return File.OpenRead(path);
+        }
+    }
+}
---
+++ Tests/NoHtml.Web.Tests.Integration/TextTransformTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NoHtml.Web.Tests.Integration
+{
+    class TextTransformTests
+    {
+    }
+}
