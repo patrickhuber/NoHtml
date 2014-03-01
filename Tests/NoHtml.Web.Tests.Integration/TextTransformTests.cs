3a1da2ce01753ad15357cd7413788bffc964a4bf
Patrick Huber <prhuber@gmail.com>
2014-03-01 15:43:46 -0500

    adds h1 test

--- Tests/NoHtml.Web.Tests.Integration/TextTransformTests.cs
+++ Tests/NoHtml.Web.Tests.Integration/TextTransformTests.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using MarkdownSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace NoHtml.Web.Tests.Integration
 {
-    class TextTransformTests
+    [TestClass]
+    public class TextTransformTests
     {
+        [TestMethod]
+        public void TextTransform_Transforms_H1()
+        {
+            ITextTransform transform = new MarkdownTextTransform(new Markdown());
+            var actual = transform.Transform("#hey");
+            var expected = "<h1>hey</h1>\n";
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
