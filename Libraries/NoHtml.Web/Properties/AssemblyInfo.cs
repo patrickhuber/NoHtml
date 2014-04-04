87727e5521310d088ac1fd30a5e7f3e2b0a17d19
patrickhuber <prhuber@gmail.com>
2014-04-04 19:51:55 -0400

    updates assembly version

--- Libraries/NoHtml.Web/Properties/AssemblyInfo.cs
+++ Libraries/NoHtml.Web/Properties/AssemblyInfo.cs
@@ -22,15 +22,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("5a598fa9-dc4b-4e3c-ab47-b6e5b9b94af1")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
