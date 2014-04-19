a09654fb0729da91f5acf7f51bb0b0e0a18db65a
Patrick Huber <prhuber@gmail.com>
2014-04-19 18:27:51 -0400

    adds cached document model and loader

---
+++ Libraries/NoHtml.Core/CachedDocumentModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NoHtml
+{
+    public class CachedDocumentModel : IQueryable<Document>
+    {
+        private IQueryable<Document> _documentCache;
+        
+        public CachedDocumentModel(IEnumerable<Document> documentIterator)
+        {
+            _documentCache = documentIterator.AsQueryable();
+        }
+
+        public IEnumerator<Document> GetEnumerator()
+        {
+            return _documentCache.GetEnumerator();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return _documentCache.GetEnumerator();
+        }
+
+        public Type ElementType
+        {
+            get { return _documentCache.ElementType; }
+        }
+
+        public System.Linq.Expressions.Expression Expression
+        {
+            get { return _documentCache.Expression; }
+        }
+
+        public IQueryProvider Provider
+        {
+            get { return _documentCache.Provider; }
+        }
+    }
+}
---
+++ Libraries/NoHtml.Core/FileSystemDocumentEnumerable.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NoHtml
+{
+    public class FileSystemDocumentEnumerable : IEnumerable<Document>
+    {
+        private IFileSystem _fileSystem;
+        private string _path;
+        private string _searchPattern;
+
+        public FileSystemDocumentEnumerable(IFileSystem fileSystem, string path, string searchPattern)
+        {
+            _fileSystem = fileSystem;
+            _path = path;
+            _searchPattern = searchPattern;
+        }
+
+        public IEnumerator<Document> GetEnumerator()
+        {
+            foreach (var document in EnumerateDocuments(_path, _searchPattern))
+            {
+                yield return document;
+            }
+
+            foreach (var directory in _fileSystem.EnumerateDirectoriesRecursive(_path, _searchPattern))
+            {
+                foreach (var document in EnumerateDocuments(directory, _searchPattern))
+                {
+                    yield return document;
+                }
+            }
+        }
+
+        private IEnumerable<Document> EnumerateDocuments(string path, string pattern)
+        {
+            foreach (var file in _fileSystem.EnumerateFiles(_path, _searchPattern))
+            {
+                using (var streamReader = new StreamReader(_fileSystem.OpenRead(file)))
+                {
+                    yield return new Document
+                    {
+                        Content = streamReader.ReadToEnd(),
+                        Path = file
+                    };
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
