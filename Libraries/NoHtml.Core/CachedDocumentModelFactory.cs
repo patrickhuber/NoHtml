6ea5057194c00c134dff92f36a977fbf870906e3
Patrick Huber <prhuber@gmail.com>
2014-04-19 18:43:40 -0400

    adds document model binder and document model factory

---
+++ Libraries/NoHtml.Core/CachedDocumentModelFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NoHtml
+{
+    public class CachedDocumentModelFactory : IDocumentModelFactory
+    {
+        private IEnumerable<Document> _documentEnumerable;
+
+        public CachedDocumentModelFactory()
+            : this(DependencyResolver.Instance.GetService<IEnumerable<Document>>())
+        { 
+        }
+
+        public CachedDocumentModelFactory(IEnumerable<Document> documentEnumerable)
+        {
+            _documentEnumerable = documentEnumerable;
+        }
+
+        public IQueryable<Document> CreateDocumentModel()
+        {
+            return new CachedDocumentModel(_documentEnumerable);
+        }
+    }
+}
---
+++ Libraries/NoHtml.Web.Mvc/QueryableDocumentModelBinder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace NoHtml.Web.Mvc
+{
+    public class QueryableDocumentModelBinder : IModelBinder
+    {
+        private IDocumentModelFactory _factory;
+
+        public QueryableDocumentModelBinder(IDocumentModelFactory factory)
+        {
+            _factory = factory;
+        }
+        
+        public QueryableDocumentModelBinder()
+            : this(NoHtml.DependencyResolver.Instance.GetService<IDocumentModelFactory>())
+        { }
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            const string SessionKey = "NoHtml.Web.Mvc.DocumentModel";
+            
+            var httpSessionBase = controllerContext.RequestContext.HttpContext.Session;
+            var queryableDocumentModel = httpSessionBase[SessionKey] as IQueryable<Document>;
+            
+            if(queryableDocumentModel == null)
+            {
+                httpSessionBase[SessionKey] = _factory.CreateDocumentModel();
+            }
+
+            return httpSessionBase[SessionKey];
+        }
+    }
+}
